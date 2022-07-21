ee7b4d08ab728ea59a255ed3b205fdbc5ab02622
WMascot <danila33366@gmail.com>
2022-07-21 19:58:47 +0300

    Created ConfigData Model

---
+++ FilesBackup/Models/ConfigData.cs
@@ -0,0 +1,10 @@
+﻿namespace FilesBackup.Models
+{
+    public class ConfigData
+    {
+        public string sourceDirectory { get; set; }
+        public string destinationDirectory { get; set; }
+        public string logLevel { get; set; }
+        public string logDirectory { get; set; }
+    }
+}

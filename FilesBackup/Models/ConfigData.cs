fc8ec6c4ea12fb1e223783fbeae3afd58d8f6dbe
WMascot <danila33366@gmail.com>
2022-07-21 20:11:24 +0300

    Added JsonProperties For ConfigData

--- FilesBackup/Models/ConfigData.cs
+++ FilesBackup/Models/ConfigData.cs
@@ -1,12 +1,17 @@
 ﻿using FilesBackup.Utils;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
 
 namespace FilesBackup.Models
 {
     public class ConfigData
     {
+        [JsonProperty(Required = Required.Always)]
         public string sourceDirectory { get; set; }
+        [JsonProperty(Required = Required.Always)]
         public string destinationDirectory { get; set; }
-        public LogLevel logLevel { get; set; }
-        public string logDirectory { get; set; }
+        [JsonConverter(typeof(StringEnumConverter))]
+        public LogLevel logLevel { get; set; } = LogLevel.None;
+        public string logDirectory { get; set; } 
     }
 }

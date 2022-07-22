6898ba3a142433986d9ca7e3a82fb58154028e05
WMascot <danila33366@gmail.com>
2022-07-22 11:58:36 +0300

    Created Log Model

---
+++ FilesBackup/Models/Log.cs
@@ -0,0 +1,15 @@
+﻿using FilesBackup.Utils;
+
+namespace FilesBackup.Models
+{
+    public class Log
+    {
+        public LogLevel logLevel { get; private set; }
+        public string logDirectory { get; private set; }
+        public Log(LogLevel logLevel, string logDirectory)
+        {
+            this.logLevel = logLevel;
+            this.logDirectory = logDirectory;
+        }
+    }
+}

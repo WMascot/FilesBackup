45fc08c88539107617b09c399a901c8eea016caa
WMascot <danila33366@gmail.com>
2022-07-22 18:22:06 +0300

    Modified and fixed methods, also added method for copying directory

--- FilesBackup/Models/Backup.cs
+++ FilesBackup/Models/Backup.cs
@@ -19,6 +19,7 @@ namespace FilesBackup.Models
             log.WriteLog("Starting Backup", LogLevel.Info);
             CreateTimeStampedDirectory();
             CopyFilesystem();
+            log.WriteLog($"Backup ended with {errorsCount} Errors", LogLevel.Debug);
         }
         private void CreateTimeStampedDirectory()
         {
@@ -28,7 +29,7 @@ namespace FilesBackup.Models
             try
             {
                 Directory.CreateDirectory(fullPath);
-                log.WriteLog($"Directory {directoryName} was successfully created", LogLevel.Info);
+                log.WriteLog($"Directory {directoryName} was successfully created", LogLevel.Debug);
                 backupDirectory = fullPath;
             }
             catch (Exception ex)
@@ -42,8 +43,47 @@ namespace FilesBackup.Models
         {
             if (backupDirectory == null || backupDirectory.Length == 0)
             {
-                log.WriteLog($"Backup failed with {errorsCount} Errors", LogLevel.Info);
+                errorsCount++;
+                log.WriteLog($"Backup failed with {errorsCount} Errors", LogLevel.Error);
                 return;
+            }
+            DirectoryInfo sourceDirectory = new DirectoryInfo(config.configData.sourceDirectory);
+            DirectoryInfo destinationDirectory = new DirectoryInfo(backupDirectory);
+            CopyDirectory(sourceDirectory, destinationDirectory);
+        }
+        private void CopyDirectory(DirectoryInfo sourceDirectory, DirectoryInfo destinationDirectory)
+        {
+            log.WriteLog($"Starting copying directory {sourceDirectory.Name}", LogLevel.Info);
+            try
+            {
+                DirectoryInfo newDirectory = Directory.CreateDirectory(Path.Combine(destinationDirectory.FullName, sourceDirectory.Name));
+
+                try
+                {
+                    foreach (var file in sourceDirectory.GetFiles())
+                    {
+                        log.WriteLog($"Starting copying file {file.Name}", LogLevel.Info);
+                        file.CopyTo(Path.Combine(newDirectory.FullName, file.Name), false);
+                        log.WriteLog($"File {file.Name} was successfully copied", LogLevel.Debug);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    errorsCount++;
+                    log.WriteLog(ex.Message, LogLevel.Error);
+                }
+
+                foreach (var subDirectory in sourceDirectory.GetDirectories())
+                {
+                    CopyDirectory(subDirectory, newDirectory);
+                }
+
+                log.WriteLog($"Directory {sourceDirectory.Name} was successfully copied", LogLevel.Debug);
+            }
+            catch (Exception ex)
+            {
+                errorsCount++;
+                log.WriteLog(ex.Message, LogLevel.Error);
             }
         }
     }

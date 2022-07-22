6a01fdea5b061640d12abb91fa3e7dfc341c7f66
WMascot <danila33366@gmail.com>
2022-07-22 12:37:47 +0300

    Created Backup Model

---
+++ FilesBackup/Models/Backup.cs
@@ -0,0 +1,15 @@
+﻿using FilesBackup.Models;
+
+namespace FilesBackup.Models
+{
+    public class Backup
+    {
+        public Config config { get; private set; }
+        public Log log { get; private set; }
+        public Backup()
+        {
+            config = new Config();
+            log = new Log(config.configData.logLevel, config.configData.logDirectory);
+        }
+    }
+}

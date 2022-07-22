971b768b2fb581b03623328f17f88f026bc03ea3
WMascot <danila33366@gmail.com>
2022-07-22 11:59:27 +0300

    Added method to Create Log File

--- FilesBackup/Models/Log.cs
+++ FilesBackup/Models/Log.cs
@@ -6,10 +6,17 @@ namespace FilesBackup.Models
     {
         public LogLevel logLevel { get; private set; }
         public string logDirectory { get; private set; }
+        public StreamWriter logWriter { get; private set; }
         public Log(LogLevel logLevel, string logDirectory)
         {
             this.logLevel = logLevel;
             this.logDirectory = logDirectory;
+            CreateLog();
+        }
+        private void CreateLog()
+        {
+            var logName = $"Log_{DateTime.Now.ToString("dd_MM_yyyy_HH_mm")}.txt";
+            logWriter = File.CreateText(logDirectory + $"\\{logName}");
         }
     }
 }

44eb14fcdb2691a89020bf41b956dceff935b3a7
WMascot <danila33366@gmail.com>
2022-07-22 18:25:26 +0300

    FInal Console application code

--- FilesBackup/Program.cs
+++ FilesBackup/Program.cs
@@ -1,1 +1,17 @@
-﻿
+﻿using FilesBackup.Models;
+
+Backup backup = new Backup();
+
+try
+{
+    backup.StartBackup();
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
+
+Console.WriteLine("Backup ended.");
+Console.WriteLine("Press any key to finish program.");
+
+Console.ReadKey();
